6cc1267195a19a27295d5f8725914ee07cb717a3
Huzaifa Arain <huzaifa.itgroup@gmail.com>
2022-03-04 02:57:38 +0500

    feat: update views

--- src/resources/views/exceptions/list.blade.php
+++ src/resources/views/exceptions/list.blade.php
@@ -5,59 +5,58 @@ $code = app()->isDownForMaintenance() ? 'maintenance' : 'error logs';
 <html lang="{!! app()->getLocale() }}">
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{ $code }}</title>
-    <link rel="stylesheet" href="//cdn.datatables.net/1.11.3/css/jquery.dataTables.min.css">
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
-        integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>{{ $code }}</title>
+  <link rel="stylesheet" href="//cdn.datatables.net/1.11.3/css/jquery.dataTables.min.css">
+  <!-- Latest compiled and minified CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+    integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
 
 </head>
 
 <body>
-    <div class="container">
-        {{$exceptions->withQueryString()->links('pagination::bootstrap-4')}}
-    
-        @foreach ($exceptions as $exception)
-            <div id="accordion-{{$exception->id}}">
-                <div class="card">
-                    <div class="card-header" id="headingOne">
-                        <h5 class="mb-0">
-                            <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne"
-                                aria-expanded="true" aria-controls="collapse-{{$exception->id}}">
-                                {{$exception->message}} - {{$exception->created_at}}
-                            </button>
-                        </h5>
-                    </div>
+  <div class="container">
+    {{ $exceptions->withQueryString()->links('pagination::bootstrap-4') }}
 
-                    <div id="collapse-{{$exception->id}}" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion-{{$exception->id}}">
-                        <div class="card-body">
-                            <?php dump($exception->context) ?>
-                        </div>
-                    </div>
-                </div>
+    @foreach ($exceptions as $exception)
+      <div id="accordion-{{ $exception->id }}">
+        <div class="card">
+          <div class="card-header" id="headingOne">
+            <h5 class="mb-0">
+              <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapse-{{ $exception->id }}">
+                {{ $exception->message }} - {{ $exception->created_at }}
+              </button>
+            </h5>
+          </div>
 
+          <div id="collapse-{{ $exception->id }}" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion-{{ $exception->id }}">
+            <div class="card-body">
+              <?php dump(json_decode($exception->context, true)); ?>
             </div>
-        @endforeach
-        {{$exceptions->withQueryString()->links('pagination::bootstrap-4')}}
-    </div>
+          </div>
+        </div>
 
-    <!-- End Error Page Content -->
-    <!--Scripts-->
-    <!-- jQuery library -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <!-- Latest compiled and minified JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
-        integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous">
-    </script>
-    <script src="//cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js"></script>
-    <script>
-        $(document).ready(function() {
-            $('#myTable').DataTable();
-        });
-    </script>
+      </div>
+    @endforeach
+    {{ $exceptions->withQueryString()->links('pagination::bootstrap-4') }}
+  </div>
+
+  <!-- End Error Page Content -->
+  <!--Scripts-->
+  <!-- jQuery library -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <!-- Latest compiled and minified JavaScript -->
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+    crossorigin="anonymous">
+  </script>
+  <script src="//cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js"></script>
+  <script>
+    $(document).ready(function() {
+      $('#myTable').DataTable();
+    });
+  </script>
 </body>
 
 </html>
